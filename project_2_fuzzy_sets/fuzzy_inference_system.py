a4690325110a596d730d22611d5fdbf989da8224
Ryan Christopher <ryandchristopher27@gmail.com>
2023-10-26 12:07:51 -0500

    Got iris classification working

--- project_2_fuzzy_sets/fuzzy_inference_system.py
+++ project_2_fuzzy_sets/fuzzy_inference_system.py
@@ -3,9 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class FuzzyInferenceSystem:
-    membership_functions = {}
+    # membership_functions = {}
 
-    def __init__(self, domain_start :float, domain_end :float, domain_step :float) -> None: 
+    def __init__(self, name :str, domain_start :float, domain_end :float, domain_step :float) -> None: 
+        self.membership_functions = {}
+
+        self.name = name
         self.domain_start = domain_start
         self.domain_end = domain_end
         self.domain_step = domain_step
@@ -13,11 +16,19 @@ class FuzzyInferenceSystem:
         self.domain = np.arange(domain_start, domain_end, domain_step)
         # self.domain = [x for x in range(domain_start, domain_end, domain_step)]
 
+    # Membership Functions
+    # ----------------------------------------------------------------------
     def create_trapezoid_mf(self, name :str, a :float, b :float, c :float, d :float) -> None:
         mf = []
-        slope_ab = 1 / (b - a)
+        if a == b:
+            slope_ab = 0
+        else:
+            slope_ab = 1 / (b - a)
         y_int_ab = 1 - (slope_ab * b)
-        slope_cd = 1 / (c - d)
+        if c == d:
+            slope_cd = 0
+        else:
+            slope_cd = 1 / (c - d)
         y_int_cd = 1 - (slope_cd * c)
         for i in self.domain:
             if i > a and i < b:
@@ -45,6 +56,27 @@ class FuzzyInferenceSystem:
 
         self.membership_functions[name] = mf
 
+
+    # Evalutation
+    # ----------------------------------------------------------------------
+    def evaluate_mamdani(self, x :float) -> {}:
+        evaluation = {}
+        for key in self.membership_functions:
+            evaluation[key] = self.membership_functions[key][self.get_mf_index(x)]
+
+        return evaluation
+    
+    # Gets max value for each data point and classifies it
+    def classify_mamdani(self, evals :[]) -> []:
+        classification = []
+        for i in evals:
+            c = max(i, key = i.get)
+            classification.append((c, i[c]))
+
+        return classification
+
+    # Plots
+    # ----------------------------------------------------------------------
     def plot_membership_functions(self) -> None:
         plt.figure(figsize=(8, 5))
         colors = ['skyblue', 'red', 'green', 'yellow', 'orange']
@@ -52,8 +84,14 @@ class FuzzyInferenceSystem:
             plt.plot(self.domain, self.membership_functions[mf], 'k')
             plt.fill_between(self.domain, self.membership_functions[mf], color=colors[i], alpha=0.4)
 
+        plt.title(f"{self.name} Plot")
         plt.ylabel('Fuzzy membership')
         plt.xlabel('The domain of interest')
         plt.ylim(-0.1, 1.1)
         plt.show()
     
+
+    # Helpers
+    # ----------------------------------------------------------------------
+    def get_mf_index(self, val :float) -> int:
+        return int(val * 10)
--- project_2_fuzzy_sets/fuzzy_sets.py
+++ project_2_fuzzy_sets/fuzzy_sets.py
@@ -1,8 +1,85 @@
+#Imports
 from fuzzy_inference_system import FuzzyInferenceSystem
+from sklearn import datasets
+import matplotlib.pyplot as plt
 
 def main():
+    iris_classification()
+
+
+def iris_classification() -> None:
+    iris = datasets.load_iris()
+    # plot_iris_data(iris)
+
+    iris_dictionary = {"sepal_length": 0, 
+                       "sepal_width": 1,
+                       "petal_length": 2,
+                       "petal_width": 3}
+
+    # Rules
+    # ------------------------------
+        # 1) If Petal Width < 0.9
+            # Petal Length < 2.5
+                # Setosa
+        # 2) If Petal Length < 4.5
+            # Petal Length > 2.5
+                # Versicolor
+             
+    # Petal Width
+    pw_fis = FuzzyInferenceSystem("Petal Width", 0, 3, 0.1)
+    pw_fis.create_trapezoid_mf("Setosa", 0, 0, 0.8, 1)
+    pw_fis.create_trapezoid_mf("Versicolor", 0.8, 0.9, 1.5, 1.8)
+    pw_fis.create_trapezoid_mf("Virginica", 1.4, 1.8, 2.8, 3)
+    # pw_fis.plot_membership_functions()
+
+    # Petal Length
+    pl_fis = FuzzyInferenceSystem("Petal Length", 0, 8, 0.1)
+    pl_fis.create_trapezoid_mf("Setosa", 0, 0, 2.3, 2.5)
+    pl_fis.create_trapezoid_mf("Versicolor", 2.5, 2.7, 4.2, 4.4)
+    pl_fis.create_trapezoid_mf("Virginica", 4.1, 4.3, 8, 8)
+    # pl_fis.plot_membership_functions()
+
+    # Iris Evaluation
+    X = iris.data
+    Y = iris.target
+
+    pw_eval = []
+    pl_eval = []
+    for i in range(X.shape[0]):
+        x = X[i]
+        
+        # Petal Width Evaluation
+        pw_eval.append(pw_fis.evaluate_mamdani(x[iris_dictionary["petal_width"]]))
+
+        # Petal Length Evaluation
+        pl_eval.append(pl_fis.evaluate_mamdani(x[iris_dictionary["petal_length"]]))
+
+    pw_class = pw_fis.classify_mamdani(pw_eval)
+    pl_class = pl_fis.classify_mamdani(pl_eval)
+
+    print("done")
+
+
+def plot_iris_data(iris) -> None:
+    features = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+    fig, axs = plt.subplots(4, 4, sharey=True, figsize=(10, 9))
+    for i in range(4):
+        for j in range(4):
+            X1 = iris.data[:,i]
+            X2 = iris.data[:,j]
+            y = iris.target
+            axs[i,j].scatter(X1, X2, c=y, cmap=plt.cm.Set1, edgecolor='k')
+            # plt.xticks(())
+            # plt.yticks(())
+            axs[i,j].set(xlabel=features[i], ylabel=features[j])
+            
+
+    plt.show()
+
+
+def test():
     start, end, step = 0, 10.5, 0.1
-    fis = FuzzyInferenceSystem(start, end, step)
+    fis = FuzzyInferenceSystem("Test", start, end, step)
 
     fis.create_trapezoid_mf("mf_1", 1, 2, 4, 5)
     # fis.create_trapezoid_mf("mf_2", 4, 5, 6, 8)
@@ -17,6 +94,7 @@ def main():
     fis.create_gaussian_mf("mf_6", 7, 1)
 
     fis.plot_membership_functions()
+
 
 if __name__=="__main__":
     main()
