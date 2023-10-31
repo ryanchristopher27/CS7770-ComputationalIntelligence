cead10f4689182fafbf767a12aa6a1293b7a9e2a
Ryan Christopher <ryandchristopher27@gmail.com>
2023-10-30 21:12:07 -0500

    96 percent accuraacy

--- project_2_fuzzy_sets/fuzzy_sets.py
+++ project_2_fuzzy_sets/fuzzy_sets.py
@@ -29,37 +29,20 @@ def iris_classification() -> None:
     fis.create_domain("PW", 0, 3, 0.1)
     fis.create_domain("PL", 0, 8, 0.1)
     fis.create_domain("Output", 0, 100, 1)
-    # fis.create_domain("Setosa", 0, 101, 1)
-    # fis.create_domain("Versicolor", 0, 101, 1)
-    # fis.create_domain("Virginica", 0, 101, 1)
-
 
     # Create Petal Width Membership Functions
     fis.create_trapezoid_mf("PW", "PW_Low", 0, 0, 0.8, 1, "i")
-    fis.create_trapezoid_mf("PW", "PW_Mid", 0.8, 0.9, 1.5, 1.8, "i")
-    fis.create_trapezoid_mf("PW", "PW_High", 1.4, 1.9, 2.8, 3, "i")
+    fis.create_trapezoid_mf("PW", "PW_Mid", 0.8, 0.9, 2, 2.2, "i")
+    fis.create_trapezoid_mf("PW", "PW_High", 1.6, 2.3, 2.8, 3, "i")
     # fis.plot_membership_functions("PW")
 
     # Create Petal Length Membership Functions
     fis.create_trapezoid_mf("PL", "PL_Low", 0, 0, 2.3, 2.5, "i")
-    fis.create_trapezoid_mf("PL", "PL_Mid", 2.5, 2.7, 4.3, 4.7, "i")
-    fis.create_trapezoid_mf("PL", "PL_High", 4.4, 4.7, 8, 8, "i")
+    fis.create_trapezoid_mf("PL", "PL_Mid", 2.5, 2.7, 4.4, 4.9, "i")
+    fis.create_trapezoid_mf("PL", "PL_High", 4.5, 4.9, 8, 8, "i")
     # fis.plot_membership_functions("PL")
 
     # Create Output Membership Functions
-        # Setosa
-    # fis.create_trapezoid_mf("Setosa", "Setosa_Low", 0, 10, 30, 40, "o")
-    # fis.create_trapezoid_mf("Setosa", "Setosa_Mid", 30, 40, 60, 70, "o")
-    # fis.create_trapezoid_mf("Setosa", "Setosa_High", 60, 70, 100, 100, "o")
-        # Versicolor
-    # fis.create_trapezoid_mf("Versicolor", "Versicolor_Low", 0, 10, 30, 40, "o")
-    # fis.create_trapezoid_mf("Versicolor", "Versicolor_Mid", 30, 40, 60, 70, "o")
-    # fis.create_trapezoid_mf("Versicolor", "Versicolor_High", 60, 70, 100, 100, "o")
-        # Virginica
-    # fis.create_trapezoid_mf("Virginica", "Virginica_Low", 0, 10, 30, 40, "o")
-    # fis.create_trapezoid_mf("Virginica", "Virginica_Mid", 30, 40, 60, 70, "o")
-    # fis.create_trapezoid_mf("Virginica", "Virginica_High", 60, 70, 100, 100, "o")
-
     fis.create_trapezoid_mf("Output", "Setosa", 0, 10, 30, 40, "o")
     fis.create_trapezoid_mf("Output", "Versicolor", 30, 40, 60, 70, "o")
     fis.create_trapezoid_mf("Output", "Virginica", 60, 70, 90, 100, "o")
@@ -73,18 +56,14 @@ def iris_classification() -> None:
         #    Petal Width Mid
                 # Versicolor
         # 3) If Petal Length High
-        #   Petal Width Hight
+        #    Petal Width Hight
                 # Virginica
 
     # Create Rules
-        # Rule 1
     fis.create_rule("Rule1", ["PW_Low", "PL_Low"], "Setosa")
-        # Rule 2
     fis.create_rule("Rule2", ["PW_Mid", "PL_Mid"], "Versicolor")
-        # Rule 3
     fis.create_rule("Rule3", ["PW_High", "PL_High"], "Virginica")
     
-
     # Iris Evaluation
     X = iris.data
     Y = iris.target
@@ -107,7 +86,7 @@ def iris_classification() -> None:
 
     print(f"Accuracy: {accuracy}%")
 
-    # plot_confusion_matrix(Y, iris_classification)
+    plot_confusion_matrix(Y, iris_classification)
 
 
 def plot_iris_data(iris) -> None:
@@ -150,25 +129,6 @@ def plot_confusion_matrix(actual :[], predicted :[]) -> None:
     cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = display_labels)
     cm_display.plot()
     plt.show() 
-
-
-def test():
-    start, end, step = 0, 10.5, 0.1
-    fis = FuzzyInferenceSystem("Test", start, end, step)
-
-    fis.create_trapezoid_mf("mf_1", 1, 2, 4, 5)
-    # fis.create_trapezoid_mf("mf_2", 4, 5, 6, 8)
-    # fis.create_trapezoid_mf("mf_3", 7, 8, 9, 10)
-
-    # fis.create_triangle_mf("mf_7", 1, 2, 4)
-    fis.create_triangle_mf("mf_8", 4, 5, 6)
-    # fis.create_triangle_mf("mf_9", 7, 8, 9)
-
-    # fis.create_gaussian_mf("mf_4", 2, 1)
-    # fis.create_gaussian_mf("mf_5", 4, 1)
-    fis.create_gaussian_mf("mf_6", 7, 1)
-
-    fis.plot_membership_functions()
 
 
 if __name__=="__main__":
